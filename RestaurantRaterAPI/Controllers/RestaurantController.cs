2dbef905377505bea45a5a3c8963495ff3cd338f
Gabriel Alvarez <galvarez2238@gmail.com>
2020-08-25 12:10:45 -0700

    Built out GET method

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,6 +15,7 @@ namespace RestaurantRaterAPI.Controllers
         private readonly RestaurantDbContext _context = new RestaurantDbContext();
 
         //-- Create (POST)
+        [HttpPost]
         public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
         {
             if (model is null)
@@ -33,8 +35,25 @@ namespace RestaurantRaterAPI.Controllers
 
         //-- Read (GET)
         // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
 
+            return NotFound();
+        }
         // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
 
         //-- Update (PUT)
 
