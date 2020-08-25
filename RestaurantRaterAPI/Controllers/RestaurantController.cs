2effe9cbd6daf9d0a70154bcaf2f36054abeee8c
Gabriel Alvarez <galvarez2238@gmail.com>
2020-08-25 11:49:48 -0700

    Added stuff

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,43 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        //-- Create (POST)
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok("You created a restaurant and it was saved!");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        //-- Read (GET)
+        // Get by ID
+
+        // Get All
+
+        //-- Update (PUT)
+
+        //-- Delete (DELETE)
+    }
+}
